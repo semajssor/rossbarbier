06fa739d19e43d7d34481501364376bd899bcde3
semajssor <141041402+semajssor@users.noreply.github.com>
2025-07-16 18:05:00 +0200

    Added poster for image on load in portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -6,10 +6,26 @@ const Portfolio = () => {
 		<section className="portfolio" id="portfolio">
 			<h2 className="portfolio-title">Portfolio</h2>
 			<div className="portfolio-grid">
-				<PortfolioCard videoSrc="/video/tchass1.MP4" ariaLabel="Video of a haircut style 1" />
-				<PortfolioCard videoSrc="/video/tchass2.MP4" ariaLabel="Video of a haircut style 2" />
-				<PortfolioCard videoSrc="/video/tchass3.MP4" ariaLabel="Video of a haircut style 3" />
-				<PortfolioCard videoSrc="/video/tchass4.MP4" ariaLabel="Video of a haircut style 4" />
+				<PortfolioCard
+					videoSrc="/video/tchass1.MP4"
+					ariaLabel="Video coupe 1"
+					posterSrc="/poster/tchass1_poster.png"
+				/>
+				<PortfolioCard
+					videoSrc="/video/tchass2.MP4"
+					ariaLabel="Video coupe 2"
+					posterSrc="/poster/tchass2_poster.png"
+				/>
+				<PortfolioCard
+					videoSrc="/video/tchass3.MP4"
+					ariaLabel="Video coupe 3"
+					posterSrc="/poster/tchass3_poster.png"
+				/>
+				<PortfolioCard
+					videoSrc="/video/tchass4.MP4"
+					ariaLabel="Video coupe 4"
+					posterSrc="/poster/tchass4_poster.png"
+				/>
 				{/* Add more video cards as needed */}
 			</div>
 		</section>
