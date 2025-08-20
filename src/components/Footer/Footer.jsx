58bec87e12a693f91665c422ff343254e30bca96
semajssor <141041402+semajssor@users.noreply.github.com>
2025-08-20 13:57:05 +0200

    added info in footer to increase SEO

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,25 @@
 import "./Footer.scss";
 import SocialIcons from "../SocialIcons/SocialIcons";
 
-
 const Footer = () => {
 	return (
 		<footer className="footer">
-			<SocialIcons />
-			<p className="footer-text">
-				&copy; {new Date().getFullYear()} Ross Barbier. Tous droits réservés.
+			<p className="phone">
+				<a href="tel:+14383793451">(438)379-3451</a>
 			</p>
+
+			<div className="socials">
+				<SocialIcons />
+				<p className="footer-text">&copy; {new Date().getFullYear()} Ross Barbier</p>
+			</div>
+
+			<address className="address">
+				308, boul. De La Marine
+				<br />
+				Varennes, Québec, J3X 2E6
+			</address>
 		</footer>
 	);
-}
+};
 
 export default Footer;
