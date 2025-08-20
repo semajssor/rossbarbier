9307727667cb5afb8692c7e8a65a4df1f2e9be44
semajssor <141041402+semajssor@users.noreply.github.com>
2025-08-20 14:11:24 +0200

    modified semantics in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,20 +4,23 @@ import SocialIcons from "../SocialIcons/SocialIcons";
 const Footer = () => {
 	return (
 		<footer className="footer">
-			<p className="phone">
-				<a href="tel:+14383793451">(438)379-3451</a>
-			</p>
+			<div className="contact-info">
+				<p className="phone">
+					<a href="tel:+14383793451" aria-label="Call Ross Barbier Salon">
+						(438) 379-3451
+					</a>
+				</p>
+				<address className="address" aria-label="Business address">
+					308, boul. De La Marine
+					<br />
+					Varennes, Québec, J3X 2E6
+				</address>
+			</div>
 
 			<div className="socials">
 				<SocialIcons />
 				<p className="footer-text">&copy; {new Date().getFullYear()} Ross Barbier</p>
 			</div>
-
-			<address className="address">
-				308, boul. De La Marine
-				<br />
-				Varennes, Québec, J3X 2E6
-			</address>
 		</footer>
 	);
 };
