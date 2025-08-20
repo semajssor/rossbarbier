704e2809e3db70a782f0a613ad041b52606bbe39
semajssor <141041402+semajssor@users.noreply.github.com>
2025-08-21 00:38:20 +0200

    update setmore link in hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,12 +7,13 @@ const Hero = () => {
 			<div className="hero-content">
 				<h2>Bienvenue chez Ross Barbier</h2>
 				<p>
-               Salut,
-               <br />
-               <br />
-               Moi c’est Hugo, jeune barbier en route vers le succès — prêt à te donner la meilleure vibe possible. Si t’as besoin d’une nouvelle tchass bien fraîche, je suis clairement ton gars.
+					Salut,
+					<br />
+					<br />
+					Moi c’est Hugo, jeune barbier en route vers le succès — prêt à te donner la meilleure vibe
+					possible. Si t’as besoin d’une nouvelle tchass bien fraîche, je suis clairement ton gars.
 				</p>
-				<Button href={"https://rossthebarber.setmore.com/"} variant="secondary">
+				<Button href={"https://rossbarbier.setmore.com"} variant="secondary">
 					Réserver maintenant
 				</Button>
 			</div>
