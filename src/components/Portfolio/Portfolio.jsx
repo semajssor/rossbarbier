472d48925b6af8c6a7d7e26bce0a857cce4b7d12
semajssor <141041402+semajssor@users.noreply.github.com>
2025-09-11 23:47:57 -0400

    changed the portfolio images/videos

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,24 +7,29 @@ const Portfolio = () => {
 			<h2 className="portfolio-title">Portfolio</h2>
 			<div className="portfolio-grid">
 				<PortfolioCard
-					videoSrc="/video/tchass1.MP4"
-					ariaLabel="Video coupe 1"
-					posterSrc="/poster/tchass1_poster.png"
+					videoSrc="/video/tchass5.mp4"
+					ariaLabel="Video coupe 5"
+					posterSrc="/poster/tchass5_poster.png"
 				/>
 				<PortfolioCard
-					videoSrc="/video/tchass2.MP4"
-					ariaLabel="Video coupe 2"
-					posterSrc="/poster/tchass2_poster.png"
+					videoSrc="/video/tchass6.mp4"
+					ariaLabel="Video coupe 6"
+					posterSrc="/poster/tchass6_poster.png"
 				/>
 				<PortfolioCard
-					videoSrc="/video/tchass3.MP4"
-					ariaLabel="Video coupe 3"
-					posterSrc="/poster/tchass3_poster.png"
+					videoSrc="/video/tchass7.mp4"
+					ariaLabel="Video coupe 7"
+					posterSrc="/poster/tchass7_poster.png"
 				/>
 				<PortfolioCard
-					videoSrc="/video/tchass4.MP4"
-					ariaLabel="Video coupe 4"
-					posterSrc="/poster/tchass4_poster.png"
+					videoSrc="/video/tchass8.mp4"
+					ariaLabel="Video coupe 8"
+					posterSrc="/poster/tchass8_poster.png"
+				/>
+				<PortfolioCard
+					videoSrc="/video/tchass9.mp4"
+					ariaLabel="Video coupe 9"
+					posterSrc="/poster/tchass9_poster.png"
 				/>
 				{/* Add more video cards as needed */}
 			</div>
