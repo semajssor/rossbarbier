f67450f868ba99faa4325939d56fb23c02a1c62c
semajssor <141041402+semajssor@users.noreply.github.com>
2026-02-05 08:03:54 +0100

    modified import google review

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero/Hero.jsx";
 import Portfolio from "./components/Portfolio/Portfolio.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import GoogleReviews from "./components/GoogleReviews/GoogleReviews.jsx";
+import GoogleReviews from "./components/GoogleReview/GoogleReview.jsx";
 
 const App = () => {
 	return (
