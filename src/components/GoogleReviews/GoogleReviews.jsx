eb18b9e5a8998faf40d872358d05b471b2b24068
semajssor <141041402+semajssor@users.noreply.github.com>
2026-02-05 11:10:32 +0100

    modified widget

--- src/components/GoogleReviews/GoogleReviews.jsx
+++ src/components/GoogleReviews/GoogleReviews.jsx
@@ -3,20 +3,18 @@ import "./GoogleReviews.scss";
 
 const GoogleReviews = () => {
 	useEffect(() => {
-		// Manually injecting the script to ensure it loads in the browser
 		const script = document.createElement("script");
 		script.src = "https://elfsightcdn.com/platform.js";
 		script.async = true;
 		document.body.appendChild(script);
 
 		return () => {
-			// Clean up the script if the component unmounts
 			document.body.removeChild(script);
 		};
 	}, []);
 
 	return (
-		<div className="reviews-container">
+		<div className="google-reviews-container">
 			<div
 				className="elfsight-app-e9330e06-27b8-4e4c-9eb4-e52c524bd34c"
 				data-elfsight-app-lazy></div>
