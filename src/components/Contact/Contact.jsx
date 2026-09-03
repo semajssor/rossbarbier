6ed0cc335031799d045d2ce42c1c53e00085a8b9
semajssor <141041402+semajssor@users.noreply.github.com>
2026-09-03 18:26:47 +0200

    modified the opening hours

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,10 +9,10 @@ const Contact = () => {
 					<br />
 					<ul>
 						<li>Lundi : Fermé</li>
-						<li>Mardi : 17h - 21h</li>
-						<li>Mercredi : 17h - 21h</li>
-						<li>Jeudi : 17h - 21h</li>
-						<li>Vendredi : 17h - 21h</li>
+						<li>Mardi : 12h - 20h</li>
+						<li>Mercredi : 12h - 20h</li>
+						<li>Jeudi : 12h - 20h</li>
+						<li>Vendredi : 10h - 17h</li>
 						<li>Samedi : 10h - 17h</li>
 						<li>Dimanche : Fermé</li>
 					</ul>
@@ -33,7 +33,7 @@ const Contact = () => {
 							src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d697.1786061077294!2d-73.423703!3d45.6565564!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc8e3a6d0dfb44b%3A0xd93bc8af086d2b54!2sRoss%20Barbier!5e0!3m2!1sen!2spl!4v1755695763827!5m2!1sen!2spl"
 							// width="400"
 							// height="300"
-							style={{border: 0}}
+							style={{ border: 0 }}
 							allowfullscreen=""
 							loading="lazy"
 							referrerpolicy="no-referrer-when-downgrade"
